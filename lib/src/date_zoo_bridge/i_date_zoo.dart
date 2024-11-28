bf7cb27ff3a25bbbedf3d226423a2ae6d623642b
bgoncharuck <dariydragon26@gmail.com>
2024-11-28 15:35:13 +0200

    feat: AnimalCalculationStrategyParams

--- lib/src/date_zoo_bridge/i_date_zoo.dart
+++ lib/src/date_zoo_bridge/i_date_zoo.dart
@@ -1,4 +1,42 @@
+import 'dart:convert';
 import 'package:ba_zi_energy/ba_zi_energy.dart';
+
+class AnimalCalculationStrategyParams {
+  final int timeZoneOffset;
+  final int sunRiseHour;
+  final int sunRiseMinute;
+
+  const AnimalCalculationStrategyParams({
+    required this.timeZoneOffset,
+    required this.sunRiseHour,
+    required this.sunRiseMinute,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'timeZoneOffset': timeZoneOffset,
+      'sunRiseHour': sunRiseHour,
+      'sunRiseMinute': sunRiseMinute,
+    };
+  }
+
+  factory AnimalCalculationStrategyParams.fromJson(Map<String, dynamic> json) {
+    return AnimalCalculationStrategyParams(
+      timeZoneOffset: json['timeZoneOffset'] as int,
+      sunRiseHour: json['sunRiseHour'] as int,
+      sunRiseMinute: json['sunRiseMinute'] as int,
+    );
+  }
+
+  String toJsonString() {
+    return jsonEncode(toJson());
+  }
+
+  factory AnimalCalculationStrategyParams.fromJsonString(String jsonString) {
+    final json = jsonDecode(jsonString) as Map<String, dynamic>;
+    return AnimalCalculationStrategyParams.fromJson(json);
+  }
+}
 
 /// ```Dart
 /// String name
@@ -6,7 +44,7 @@ import 'package:ba_zi_energy/ba_zi_energy.dart';
 /// ```
 abstract class AnimalCalculationStrategy {
   String get name;
-  AnimalSynergy calculate(DateTime dt, int timeZoneOffset);
+  AnimalSynergy calculate(DateTime dt, AnimalCalculationStrategyParams params);
 }
 
 /// ```Dart
