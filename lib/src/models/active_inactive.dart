614d43fb8ec6dffb649bd40e59fd51fbd232f3ba
Bohdan Honcharuk <dariydragon26@gmail.com>
2025-05-20 10:40:20 +0300

    Update active_inactive.dart

--- lib/src/models/active_inactive.dart
+++ lib/src/models/active_inactive.dart
@@ -1,5 +1,41 @@
 import 'package:ba_zi_energy/ba_zi_energy.dart';
 
+const energyFusions = [
+  {Energy.FireYang, Energy.MetalYin},
+  {Energy.WaterYang, Energy.FireYin},
+  {Energy.EarthYang, Energy.WaterYin},
+  {Energy.WoodYang, Energy.EarthYin},
+  {Energy.MetalYang, Energy.WoodYin},
+];
+
+class TwoAnimalGroups {
+  final List<Animal> first;
+  final List<Animal> second;
+  const TwoAnimalGroups({required this.first, required this.second});
+}
+
+class PartnersFusionResult {
+  final List<Iterable<Animal>> matches;
+  final List<Animal> remainingFirst;
+  final List<Animal> remainingSecond;
+  const PartnersFusionResult({
+    required this.matches,
+    required this.remainingFirst,
+    required this.remainingSecond,
+  });
+}
+
+class PartnersEnergyFusionResult {
+  final List<Iterable<Energy>> matches;
+  final List<Energy> remainingFirst;
+  final List<Energy> remainingSecond;
+  const PartnersEnergyFusionResult({
+    required this.matches,
+    required this.remainingFirst,
+    required this.remainingSecond,
+  });
+}
+
 class ActiveAndInactiveAnimals {
   const ActiveAndInactiveAnimals({
     required this.active,
