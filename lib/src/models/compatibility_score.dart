affa553c78405b765895455f4ef77fc28e6d7cbe
Bohdan Honcharuk <dariydragon26@gmail.com>
2025-05-27 12:56:03 +0300

    Update compatibility_score.dart

--- lib/src/models/compatibility_score.dart
+++ lib/src/models/compatibility_score.dart
@@ -1,9 +1,9 @@
 enum ZooCompatibilityScore {
   problematic(275),
   needsWork(400),
-  compatible(550),
+  compatible(600),
   excellent(700),
-  perfect(850);
+  perfect(900);
 
   final int score;
   const ZooCompatibilityScore(this.score);
